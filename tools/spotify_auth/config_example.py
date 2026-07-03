c9ef2580a97e48bc4233d4e1616429a480233c3f
phoeher <52426003+phoeher@users.noreply.github.com>
2026-07-02 22:45:34 -0400

    remove local spotify credentials from repository

--- tools/spotify_auth/config_example.py
+++ tools/spotify_auth/config_example.py
@@ -1,7 +1,7 @@
 # Spotify API Credentials from Developer Dashboard
 
-CLIENT_ID = "9dbeadc033564697a540668e136a5481"
-CLIENT_SECRET = "517ec2e98fe44c8b9d1b11545082ef17"
+CLIENT_ID = "your client id here"
+CLIENT_SECRET = "your client secret here"
 
 REDIRECT_URI = "http://127.0.0.1:8080/callback"
 
