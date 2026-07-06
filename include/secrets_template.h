3e954af46cec3bee564856ca4d217f83c0c38360
phoeher <52426003+phoeher@users.noreply.github.com>
2026-07-06 15:27:02 -0400

    Implement ESP32 WiFi connection and basic http get request

---
+++ include/secrets_template.h
@@ -0,0 +1,7 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* WIFI_SSID = "YourWiFiName";
+const char* WIFI_PASSWORD = "YourPassword";
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
 #include <iostream>
 #include <string>
+#include <WiFi.h>
+#include "secrets.h"
+#include <HTTPClient.h>
 
 // initialize variables here:
 
 // put function declarations here:
-void requestAuthorization();
+void connectToWiFi();
+void testInternetConnection();
 
+// put your setup code here, to run once:
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(11500);
+
+  connectToWiFi();
+  testInternetConnection();
 }
 
+// put your main code here, to run repeatedly:
 void loop() {
-  // put your main code here, to run repeatedly:
+  //placeholder for main loop code
 }
 
 // put function definitions here:
+void connectToWiFi() {
+  Serial.println("connecting to WiFi...");
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void testInternetConnection() {
+  HTTPClient http;
+  http.begin("http://api.ipify.org");
+  int httpResponseCode = http.GET();
+  String payload = http.getString();
+  Serial.println(payload);
+  http.end();
+}
